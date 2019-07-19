5c9a3fa237ff9fa52d363e29f9c396299a86eba7
june0725 <june0725@PN-201901603-01.hsnc.hanwha.com>
2019-07-19 17:27:30 +0900

    최초생성

---
+++ camelSplitterExample/src/main/java/com/javarticles/camel/split/OrderItem.java
@@ -0,0 +1,45 @@
+package com.javarticles.camel.split;
+
+public class OrderItem {
+    private Order order;
+    private String itemName;
+    private int quantity;
+    private int price;
+    private boolean processed;
+    
+    public OrderItem(Order order, String itemName, int quantity, int price) {
+        this.order = order;
+        this.itemName = itemName;
+        this.quantity = quantity;
+        this.price = price;
+    }
+
+    public Order getOrder() {
+        return order;
+    }
+
+    public String getItemName() {
+        return itemName;
+    }
+    
+    public void process() {
+        processed = true;
+    }
+
+    public boolean isProcessed() {
+        return processed;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+    
+    public String toString() {
+        return "Item[" + itemName + ", total price: " + (quantity * price) + ", processed: " + isProcessed() + "]";
+    }
+    
+}
---
+++ camelSplitterExample/src/main/java/com/javarticles/camel/split/OrderItemProcessor.java
@@ -0,0 +1,7 @@
+package com.javarticles.camel.split;
+
+public class OrderItemProcessor {
+    public void process(OrderItem item) {
+        item.process();
+    }
+}
